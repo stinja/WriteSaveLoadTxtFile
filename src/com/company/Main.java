ee2fd42702a73d08e29362d40a6107acbfdfe5cf
stinja <projectmaydev@gmail.com>
2021-09-18 16:01:47 -0500

    add TODOs and edited some comments, no code changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 import java.io.*;
 
 
-/* ---- Quote listing -------------------------------------------
+/** --- QUOTESAVE by STINJA  -----------------
 Add/Save/Load quotes using a master text file.
 To avoid accidental overwrite, have program create a back up file
 and save file to a folder called backups.
-------------------------------------------------------------  **/
+ ---------------------------------------------  **/
 
 public class Main {
 
-    /* ---- Start Menu -----------------------------------------
+    /** --- Start Menu ------------------------
     Start menu that runs at the beginning of the program.
     Anytime an action is completed display this menu.
     This should be the only way to exit in program.
-    ------------------------------------------------------------  **/
+    -------------------------------------------  **/
     static int startMenu(Scanner menuInput) {
         // output a bunch of text
         System.out.println("QUOTESAVE");
@@ -39,24 +39,55 @@ public class Main {
         return menuSelect;
     }
 
+    //TODO refactor and use methods
+    /** Methods for later
+    static void setIndex() {
+
+    }
+
+    static void setQuote() {
+
+    }
+
+    static void setAuthor() {
+
+    }
+
+    static void writeQuoteToFile(int index, String quote, String author) {
+
+    }
+
+    static void printFileToScreen() {
+
+         // Read the file into a character array
+         FileReader fileReader = new FileReader(file);
+         char [] someChar = new char[250];
+         fileReader.read(someChar); // reads the content to the someChar array
+
+         // Output the file to the screen
+         for (char c : someChar) // some sort of "enhanced for loop"
+         System.out.print(c);
+         fileReader.close();
+
+    } **/
+
     public static void main(String[] args) throws IOException {
 
-        //TODO refactor and use methods
-        //TODO add exception handling
+
         //TODO create a menu
 
         File file = new  File("quotesList.txt"); // create the file
-        file.createNewFile();
+        file.createNewFile(); //TODO add check and only run if a quote file doesnt exist
 
         FileWriter fileWriter = new FileWriter(file, true);
 
         Scanner input = new Scanner(System.in);
 
-        int menuSelect = startMenu(input);
+        int menuSelect = startMenu(input); //TODO make menu work and do something with menu selection
 
         // set index
         System.out.print("Enter quote index: ");
-        int quoteIndex = input.nextInt();
+        int quoteIndex = input.nextInt(); //TODO automate and increment based on previous quote index
         input.nextLine(); // advance scanner due to scanner bug
 
         // write index
@@ -65,7 +96,7 @@ public class Main {
 
         // set quote
         System.out.print("Enter quote: ");
-        String quoteContent = input.nextLine();
+        String quoteContent = input.nextLine(); //TODO add exception handling for quote input
 
         // write quote
         fileWriter.write("\n\"" + quoteContent + "\"");
@@ -73,7 +104,7 @@ public class Main {
 
         // set author
         System.out.print("Enter author: ");
-        String quoteAuthor = input.nextLine();
+        String quoteAuthor = input.nextLine(); //TODO add exception handling for author input
 
         // write author, add line after
         fileWriter.write("\nby " + quoteAuthor + "\n");
@@ -81,17 +112,6 @@ public class Main {
 
         // add line and close
         fileWriter.close();
-
-        // Read the file into a character array
-        FileReader fileReader = new FileReader(file);
-        char [] someChar = new char[250];
-        fileReader.read(someChar); // reads the content to the someChar array
-
-        /**
-        // Output the file to the screen
-        for (char c : someChar) // some sort of "enhanced for loop"
-            System.out.print(c);
-        fileReader.close(); **/
 
     }
 }
